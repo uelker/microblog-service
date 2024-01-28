043146269546d00c2a8cdc06693f1de9f2c242cb
Enes Ülker <enes.uelker@hotmail.com>
2024-01-28 20:34:06 +0100

    using dtos for mapping in repository

--- src/main/java/uelker/microblog/repository/PostRepository.java
+++ src/main/java/uelker/microblog/repository/PostRepository.java
@@ -3,7 +3,14 @@ package uelker.microblog.repository;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
+import uelker.microblog.dto.CreatePostDto;
+import uelker.microblog.dto.UpdatePostDto;
 import uelker.microblog.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static java.util.UUID.randomUUID;
 
 @Repository
 public class PostRepository {
@@ -13,16 +20,33 @@ public class PostRepository {
         this.postTable = postTable;
     }
 
-    public Post createPost(Post post) {
+    public Post createPost(CreatePostDto postDto) {
+        Post post = Post.builder()
+                .id(randomUUID().toString())
+                .title(postDto.title())
+                .content(postDto.content())
+                .status(postDto.status())
+                .createdAt(LocalDateTime.now())
+                .build();
+
         this.postTable.putItem(post);
         return post;
     }
 
-    public Post getPost(String postId) {
-        return this.postTable.getItem(Key.builder().partitionValue(postId).build());
+    public Optional<Post> getPost(String postId) {
+        Post post = this.postTable.getItem(Key.builder().partitionValue(postId).build());
+        return Optional.ofNullable(post);
     }
 
-    public Post updatePost(Post post) {
+    public Post updatePost(String id, UpdatePostDto postDto) {
+        Post post = Post.builder()
+                .id(id)
+                .title(postDto.title())
+                .content(postDto.content())
+                .status(postDto.status())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
         return this.postTable.updateItem(r -> r.item(post).ignoreNulls(Boolean.TRUE));
     }
 
