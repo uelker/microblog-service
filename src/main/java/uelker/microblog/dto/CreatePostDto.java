7a18e358b1b495ae5119aa161d4e6a49b755e239
Enes Ülker <enes.uelker@hotmail.com>
2024-02-01 15:05:05 +0100

    adding validation dependency

--- src/main/java/uelker/microblog/dto/CreatePostDto.java
+++ src/main/java/uelker/microblog/dto/CreatePostDto.java
@@ -1,12 +1,17 @@
 package uelker.microblog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import uelker.microblog.model.Status;
 
 public record CreatePostDto
         (
+                @NotBlank(message = "Title cannot be blank")
                 String title,
 
+                @NotBlank(message = "Content cannot be blank")
                 String content,
 
+                @NotNull
                 Status status) {
 }
