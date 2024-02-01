367d48ea4483f1b688bc0ac667122b81ed130852
Enes Ülker <enes.uelker@hotmail.com>
2024-02-01 15:07:40 +0100

    change some configurations for dynamo db

--- src/main/java/uelker/microblog/config/DynamoDbConfiguration.java
+++ src/main/java/uelker/microblog/config/DynamoDbConfiguration.java
@@ -9,18 +9,23 @@ import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
-import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.utils.StringUtils;
 import uelker.microblog.model.Post;
+import uelker.microblog.model.Status;
+
+import java.time.LocalDateTime;
+
+import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.primaryPartitionKey;
 
 @Configuration
 public class DynamoDbConfiguration {
-    @Value("${aws.accessKey}")
+    @Value("${aws.accessKey:}")
     private String accessKey;
 
-    @Value("${aws.secretKey}")
+    @Value("${aws.secretKey:}")
     private String secretKey;
 
     @Value("${aws.region}")
@@ -43,9 +48,15 @@ public class DynamoDbConfiguration {
                 .region(Region.of(region)).credentialsProvider(credentialsProvider).build();
     }
 
-
     @Bean
     public DynamoDbTable<Post> postTable(DynamoDbEnhancedClient dynamoClient) {
-        return dynamoClient.table(this.postTableName, TableSchema.fromBean(Post.class));
+        return dynamoClient.table(this.postTableName, StaticTableSchema.builder(Post.class)
+                .newItemSupplier(Post::new).addAttribute(String.class, a -> a.name("id").getter(Post::getId).setter(Post::setId).tags(primaryPartitionKey()))
+                .addAttribute(String.class, a -> a.name("title").getter(Post::getTitle).setter(Post::setTitle))
+                .addAttribute(String.class, a -> a.name("content").getter(Post::getContent).setter(Post::setContent))
+                .addAttribute(Status.class, a -> a.name("status").getter(Post::getStatus).setter(Post::setStatus))
+                .addAttribute(LocalDateTime.class, a -> a.name("createdAt").getter(Post::getCreatedAt).setter(Post::setCreatedAt))
+                .addAttribute(LocalDateTime.class, a -> a.name("updatedAt").getter(Post::getUpdatedAt).setter(Post::setUpdatedAt))
+                .build());
     }
 }
